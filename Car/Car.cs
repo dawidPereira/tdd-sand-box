c4ad824b91e7fa8358d232076bf23b159c126f18
Dawid Pereira <pereiradawid@outlook.com>
2021-03-20 17:24:51 +0100

    Green

---
+++ Car/Car.cs
@@ -0,0 +1,10 @@
+﻿namespace Car
+{
+    public class Car
+    {
+        public Car(Color color, string name, double fuelUsage, int tankCapacity, double fuelAmount)
+        {
+
+        }
+    }
+}
---
+++ Car/Color.cs
@@ -0,0 +1,7 @@
+﻿namespace Car
+{
+    public enum Color
+    {
+        Red,
+    }
+}
