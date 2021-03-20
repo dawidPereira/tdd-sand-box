8c91e7d9dfb28759a1fc8b2932e771127ec6da1d
Dawid Pereira <pereiradawid@outlook.com>
2021-03-20 17:20:54 +0100

    Red

---
+++ Car.Tests/CarTests.cs
@@ -0,0 +1,13 @@
+﻿using Xunit;
+
+namespace Car.Tests
+{
+    public class CarTests
+    {
+        [Fact]
+        public void Create_WhenValid_ShouldCreateCar()
+        {
+            var car = new Car(Color.Red, "Ford Mondeo", 5.0, 60, 10);
+        }
+    }
+}
