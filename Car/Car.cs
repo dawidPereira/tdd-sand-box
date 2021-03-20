a7a245eb9f63c5b41a61efba1110a6bd2afac108
Dawid Pereira <pereiradawid@outlook.com>
2021-03-20 17:39:49 +0100

    Green

--- Car/Car.cs
+++ Car/Car.cs
@@ -2,19 +2,17 @@
 {
     public class Car
     {
-        public Car(Color color, string name, double fuelUsage, int tankCapacity, double fuelAmount)
+        public Car(Color color, string name, double fuelUsage, int tankCapacity)
         {
             Color = color;
             Name = name;
             FuelUsage = fuelUsage;
             TankCapacity = tankCapacity;
-            FuelAmount = fuelAmount;
         }
 
         public Color Color { get; }
         public string Name { get; }
         public double FuelUsage { get; }
         public int TankCapacity { get; }
-        public double FuelAmount { get; }
     }
 }
